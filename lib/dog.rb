28d766e75db5f6a044b9ba938814959839682cfe
Hanna Lee <mani21na@gmail.com>
2019-08-23 14:03:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
 # Add your code here
+require 'pry'
+
+class Dog 
+  #setters, getters
+  attr_accessor :name, :save
+  
+  #class variables
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+#    @@all << self
+#binding.pry
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.print_all
+    @@all.find{|x| puts x.name}
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+end
+
+dog1 = Dog.new("A")
+dog2 = Dog.new("B")
+Dog.print_all
